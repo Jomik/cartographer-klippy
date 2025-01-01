9d327df25755302cd02d8ca25fe50be18ebbffd7
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-01-01 15:19:50 +0100

    refactor: remove some final annotations

--- typings/kinematics/extruder.pyi
+++ typings/kinematics/extruder.pyi
@@ -1,8 +1,6 @@
 # https://github.com/Klipper3d/klipper/blob/master/klippy/kinematics/extruder.py
-from typing import final
 from extras.heaters import Heater
 
-@final
 class Extruder:
     def get_name(self) -> str: ...
     def get_heater(self) -> Heater: ...
