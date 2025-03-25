d8b807a27ff80c9c991895812d1764e7693fe79a
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-03-25 19:31:35 +0100

    feat: start of configuration (#40)

---
+++ src/cartographer/configuration.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from cartographer.probes.scan_model import Configuration as ScanModelConfiguration
+
+
+class ProbeMode(Enum):
+    SCAN = "scan"
+    TOUCH = "touch"
+
+
+class CartographerConfiguration(Protocol):
+    x_offset: float
+    y_offset: float
+    backlash_compensation: float
+    homing_mode: ProbeMode
+    probe_mode: ProbeMode
+    verbose: bool
+
+    scan_models: dict[str, ScanModelConfiguration]
