d55a01df8bf7990fcce1f6f2442d2baeb809d293
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-04-05 21:43:42 +0200

    fix(mcu): wait for initialise (#54)

--- src/cartographer/klipper/mcu/constants.py
+++ src/cartographer/klipper/mcu/constants.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 import logging
 import struct
-from typing import TypedDict, final
+from typing import TYPE_CHECKING, TypedDict, final
 
 from extras.thermistor import Thermistor
-from mcu import MCU, CommandQueryWrapper
-from mcu import TriggerDispatch as KlipperTriggerDispatch
+
+if TYPE_CHECKING:
+    from mcu import MCU, CommandQueryWrapper
 
 logger = logging.getLogger(__name__)
 
@@ -25,16 +26,15 @@ UINT16_MAX = 0xFFFF
 
 @final
 class KlipperCartographerConstants:
-    _sensor_frequency: float = 0.0
+    _sensor_frequency: float = 1
     _inverse_adc_max: float = 0.0
-    _adc_smooth_count: int | None = None
-    _adc_count: int | None = None
+    _adc_smooth_count: int = 1
 
+    minimum_adc_count: int = 0
     minimum_count: int = 0
 
     def __init__(self, mcu: MCU):
         self._mcu = mcu
-        self._dispatch = KlipperTriggerDispatch(self._mcu)
         self._command_queue = self._mcu.alloc_command_queue()
         self._mcu.register_config_callback(self._initialize_constants)
 
@@ -68,7 +68,8 @@ class KlipperCartographerConstants:
         if f_count >= UINT32_MAX or adc_count >= UINT16_MAX:
             msg = "invalid f_count or adc_count"
             raise self._mcu.error(msg)
-        self._adc_count = adc_count
+
+        self.minimum_adc_count = adc_count
         self.minimum_count = f_count
 
     def _clock_to_sensor_frequency(self, clock_frequency: float) -> float:
@@ -85,7 +86,5 @@ class KlipperCartographerConstants:
         return int(frequency * (2**28) / self._sensor_frequency)
 
     def calculate_temperature(self, raw_temp: int) -> float:
-        if self._adc_smooth_count is None:
-            return 0.0
         temp_adc = raw_temp / self._adc_smooth_count * self._inverse_adc_max
         return self.thermistor.calc_temp(temp_adc)
