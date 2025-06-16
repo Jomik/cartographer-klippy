134e69945d906dcd05ae5e5468f9d7cab43e2f1a
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-06-16 15:50:58 +0200

    fix(kalico): correct probe interface (#190)

--- src/cartographer/adapters/kalico/probe.py
+++ src/cartographer/adapters/kalico/probe.py
@@ -3,9 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, final
 
 from gcode import CommandError, GCodeCommand
-from typing_extensions import override
 
-from cartographer.adapters.klipper.probe import KlipperCartographerProbe, KlipperProbeSession
 from cartographer.adapters.utils import reraise_as
 
 if TYPE_CHECKING:
@@ -15,7 +13,7 @@ if TYPE_CHECKING:
 
 
 @final
-class KalicoCartographerProbe(KlipperCartographerProbe):
+class KalicoCartographerProbe:
     def __init__(
         self,
         toolhead: KlipperToolhead,
@@ -23,28 +21,41 @@ class KalicoCartographerProbe(KlipperCartographerProbe):
         probe_macro: ProbeMacro,
         query_probe_macro: QueryProbeMacro,
     ) -> None:
-        super().__init__(toolhead, probe, probe_macro, query_probe_macro)
+        self.probe = probe
+        self.probe_macro = probe_macro
+        self.query_probe_macro = query_probe_macro
+        self.toolhead = toolhead
+
+        self.lift_speed = 5.0
         self.sample_count = 1
         self.samples_tolerance = 0.1
         self.samples_retries = 0
-        self._probe_session = KlipperProbeSession(probe, toolhead)
+
+    def get_offsets(self) -> tuple[float, float, float]:
+        return self.probe.offset.as_tuple()
+
+    def get_status(self, eventtime: float):
+        del eventtime
+        return {
+            "name": "cartographer",
+            "last_query": 1 if self.query_probe_macro.last_triggered else 0,
+            "last_z_result": self.probe_macro.last_trigger_position or 0,
+        }
+
+    def get_lift_speed(self, gcmd: GCodeCommand | None = None):
+        if gcmd is not None:
+            return gcmd.get_float("LIFT_SPEED", self.lift_speed, above=0.0)
+        return self.lift_speed
+
+    @reraise_as(CommandError)
+    def run_probe(self, gcmd: GCodeCommand) -> list[float]:
+        del gcmd
+        pos = self.toolhead.get_position()
+        trigger_pos = self.probe.perform_probe()
+        return [pos.x, pos.y, trigger_pos]
 
     def multi_probe_begin(self):
         pass
 
     def multi_probe_end(self):
         pass
-
-    @override
-    def start_probe_session(self, gcmd: GCodeCommand) -> KlipperProbeSession:
-        return self._probe_session
-
-    @reraise_as(CommandError)
-    def run_probe(self, gcmd: GCodeCommand) -> None:
-        return self._probe_session.run_probe(gcmd)
-
-    def pull_probed_results(self):
-        return self._probe_session.pull_probed_results()
-
-    def end_probe_session(self) -> None:
-        return self._probe_session.end_probe_session()
