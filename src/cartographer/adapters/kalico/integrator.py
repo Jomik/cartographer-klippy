f546d1af3918f03b8314c8c39af492332b90575c
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-06-16 10:44:53 +0200

    fix(kalico): align probe interfaces

--- src/cartographer/adapters/kalico/integrator.py
+++ src/cartographer/adapters/kalico/integrator.py
@@ -5,24 +5,24 @@ from typing import TYPE_CHECKING, final
 
 from typing_extensions import override
 
+from cartographer.adapters.kalico.probe import KalicoCartographerProbe
 from cartographer.adapters.klipper.mcu import KlipperCartographerMcu
-from cartographer.adapters.klipper.probe import KlipperCartographerProbe
 from cartographer.adapters.klipper.toolhead import KlipperToolhead
 from cartographer.adapters.klipper_like.integrator import KlipperLikeIntegrator
 from cartographer.macros.probe import ProbeMacro, QueryProbeMacro
 
 if TYPE_CHECKING:
-    from cartographer.adapters.klipper.adapters import KlipperAdapters
+    from cartographer.adapters.kalico.adapters import KalicoAdapters
     from cartographer.core import PrinterCartographer
 
 logger = logging.getLogger(__name__)
 
 
 @final
-class KlipperIntegrator(KlipperLikeIntegrator):
-    def __init__(self, adapters: KlipperAdapters) -> None:
-        assert isinstance(adapters.mcu, KlipperCartographerMcu), "Invalid MCU type for KlipperIntegrator"
-        assert isinstance(adapters.toolhead, KlipperToolhead), "Invalid toolhead type for KlipperIntegrator"
+class KalicoIntegrator(KlipperLikeIntegrator):
+    def __init__(self, adapters: KalicoAdapters) -> None:
+        assert isinstance(adapters.mcu, KlipperCartographerMcu), "Invalid MCU type for KalicoIntegrator"
+        assert isinstance(adapters.toolhead, KlipperToolhead), "Invalid toolhead type for KalicoIntegrator"
         super().__init__(adapters)
 
     @override
@@ -32,7 +32,7 @@ class KlipperIntegrator(KlipperLikeIntegrator):
 
         self._printer.add_object(
             "probe",
-            KlipperCartographerProbe(
+            KalicoCartographerProbe(
                 self._toolhead,
                 cartographer.scan_mode,
                 probe_macro,
